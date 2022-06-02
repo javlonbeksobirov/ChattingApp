0e684085c6da02e932ae5395d2c9855d7a0e7e51
Javlonbek <javlonbeksobirov03@gmail.com>
2022-06-02 22:48:08 +0500

    add image chooser in sign in

--- app/src/main/java/com/flora/chattingapp/LoginActivity.java
+++ app/src/main/java/com/flora/chattingapp/LoginActivity.java
@@ -1,15 +1,28 @@
 package com.flora.chattingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText editTextEmail, editTextPassword;
     private Button buttonSignIn, buttonSingUp;
+    private TextView forgetPassword;
+
+    FirebaseAuth auth;
 
 
     @Override
@@ -24,5 +37,55 @@ public class LoginActivity extends AppCompatActivity {
         editTextPassword = findViewById(R.id.edt_password);
         buttonSignIn = findViewById(R.id.btn_sign_in);
         buttonSingUp = findViewById(R.id.btn_sign_up);
+        forgetPassword = findViewById(R.id.tv_forget_password);
+
+
+        buttonSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+
+                if (!email.equals("") && !password.equals("")){
+                    signIn(email, password);
+                }else {
+                    Toast.makeText(LoginActivity.this,"Please enter an email and password!!", Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
+
+        forgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, PasswordResetActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void signIn(String email, String password){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Toast.makeText(LoginActivity.this, "Sign In is succesfully", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this,"Sign In is not succesfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/flora/chattingapp/SignUpActivity.java
+++ app/src/main/java/com/flora/chattingapp/SignUpActivity.java
@@ -1,18 +1,25 @@
 package com.flora.chattingapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.imageview.ShapeableImageView;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText editTextEmailSignUp, editTextPasswordSignUp, editTextUserNameSignUp;
     private Button buttonRegister;
     private ShapeableImageView profileImage;
+
+    private Boolean imageControl = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +35,48 @@ public class SignUpActivity extends AppCompatActivity {
         editTextUserNameSignUp = findViewById(R.id.edt_username_sign_up);
         buttonRegister = findViewById(R.id.btn_sign_up);
         profileImage = findViewById(R.id.profileImage);
+
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+            }
+        });
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = editTextEmailSignUp.getText().toString();
+                String password = editTextPasswordSignUp.getText().toString();
+                String userName = editTextUserNameSignUp.getText().toString();
+
+                if (!email.equals("") && !password.equals("") && !userName.equals("")){
+                    signUp(email, password, userName);
+                }
+            }
+        });
+    }
+
+    public void imageChooser(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null){
+            Uri imageUri = data.getData();
+            Glide.with(this).load(imageUri).into(profileImage);
+            imageControl = true;
+        }   else {
+            imageControl = false;
+        }
+    }
+
+    public void signUp(String email, String password, String userName){
+
     }
 }
